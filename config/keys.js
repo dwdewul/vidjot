f421476235aba95eb20ee6deb7e582544358c2fa
David DeWulf <dwdewul@gmail.com>
2017-10-02 08:50:27 -0500

    Fix require bug in config/keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,10 @@
-const prod = require('./prod');
-const dev = require('./dev');
-
 if(process.env.NODE_ENV === 'production'){
+    const prod = require('./prod');
     module.exports ={
         mongoURI: prod.mongoURI
     } 
 } else {
+    const dev = require('./dev');
     module.exports = {
         mongoURI: dev.mongoURI
     }
