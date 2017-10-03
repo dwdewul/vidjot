1a7f83389dfdc13bf1590b52dc6f32a7b4e79d46
David DeWulf <dwdewul@gmail.com>
2017-10-03 18:11:24 -0500

    Final tweaks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
